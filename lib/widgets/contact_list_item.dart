467ff0f1e6b391b7270526b05518bc894cbd3a2d
james_feed <jatsadakorn555@gmail.com>
2020-10-27 21:28:53 +0700

    Widget of Contact Page

---
+++ lib/widgets/contact_list_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ContactListItem extends StatelessWidget {
+
+  final String title;
+  final String contact;
+  final IconData icon;
+  final Function onPressed;
+  final Color backgroundColor;
+  final Color foregroundColor;
+  final String tooltip;
+
+  ContactListItem({
+    @required this.title,
+    @required this.contact,
+    @required this.icon,
+    @required this.onPressed,
+    this.backgroundColor,
+    this.foregroundColor,
+    this.tooltip
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              contact,
+              style:GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(width: 16,),
+            FloatingActionButton(
+              onPressed: onPressed,
+              tooltip: tooltip,
+              mini: true,
+              elevation: 0,
+              hoverElevation: 4,
+              focusElevation: 2,
+              highlightElevation: 2,
+              backgroundColor: backgroundColor,
+              foregroundColor: foregroundColor,
+              splashColor: foregroundColor.withOpacity(0.5),
+              hoverColor: foregroundColor.withOpacity(0),
+              focusColor: foregroundColor.withOpacity(0.15),
+              child: Icon(icon),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
