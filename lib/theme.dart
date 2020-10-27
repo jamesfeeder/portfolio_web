8e74bbcd716606862f1523e33df3e4dcb018bf39
james_feed <jatsadakorn555@gmail.com>
2020-10-27 21:26:30 +0700

    Add new ThemeData

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WebTheme {
+
   static double appBarHeight = 88;
   
   static ThemeData themeData = ThemeData(
-    fontFamily: "Raleway",
-    primarySwatch: Colors.blue,
+    textTheme: GoogleFonts.ralewayTextTheme(),
+    cardTheme: CardTheme(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))
+    ),
+    buttonTheme: ButtonThemeData(
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))
+    )
   );
+
+  static EdgeInsets defaultPagePadding = EdgeInsets.symmetric(horizontal: 88, vertical: 48);
+  static EdgeInsets defaultItemPadding = EdgeInsets.all(16);
 }
