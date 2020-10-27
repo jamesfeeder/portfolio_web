4fca4ef2ea8ee663f77147c201e7ca1cb373038a
james_feed <jatsadakorn555@gmail.com>
2020-10-27 21:28:30 +0700

    Use Google Fonts

--- lib/page/tabs/tab_home.dart
+++ lib/page/tabs/tab_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/theme.dart';
 
 class HomeTab extends StatelessWidget {
 
@@ -15,7 +17,7 @@ class HomeTab extends StatelessWidget {
     return Container(
       height: MediaQuery.of(context).size.height - 64,
       child: Padding(
-        padding: EdgeInsets.all(88),
+        padding: WebTheme.defaultPagePadding,
         child: RepaintBoundary(
           child: Column(
             mainAxisSize: MainAxisSize.max,
@@ -24,7 +26,7 @@ class HomeTab extends StatelessWidget {
             children: [
               Text(
                 "Jetsadakorn Maliwan".toUpperCase(),
-                style: TextStyle(fontFamily: "Raleway", color: codeBlack, fontSize: titleFontSize, fontWeight: FontWeight.w700),
+                style: GoogleFonts.raleway(color: codeBlack, fontSize: titleFontSize, fontWeight: FontWeight.w700),
               ),
               Text(
                 "\n// Mobile Apps Developer\n// Flutter\n",
