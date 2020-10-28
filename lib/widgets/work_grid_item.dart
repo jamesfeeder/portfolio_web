2a38a43c6424b1b98b891729ef723c74c7782b26
jamesfeeder <jatsadakorn555@gmail.com>
2020-10-29 04:15:17 +0700

    .

--- lib/widgets/work_grid_item.dart
+++ lib/widgets/work_grid_item.dart
@@ -7,6 +7,10 @@ class WorkGridItem extends StatefulWidget {
 
 class _WorkGridItemState extends State<WorkGridItem> {
 
+  // Card Var
+  double _elevation = 1;
+
+  // Overlay Var
   double _opacity = 0;
   int _transDuration = 500;
   double _left = 0;
@@ -14,91 +18,98 @@ class _WorkGridItemState extends State<WorkGridItem> {
 
   @override
   Widget build(BuildContext context) {
+    int _gridCol = MediaQuery.of(context).size.width~/400;
+    double _cardWidth = (MediaQuery.of(context).size.width-(88*2)+((_gridCol-1)*16))/_gridCol;
+    double _cardHeight = _cardWidth*(2/3);
     return MouseRegion(
       onEnter: (value) {
         setState(() {
+          _elevation = 4;
           _opacity = 1;
-          // _transDuration = 0;
-          // _left = value.localPosition.dx+5;
-          // _top = value.localPosition.dy+5;
-        });
-      },
-      onExit: (value) {
-        setState(() {
-          _opacity = 0;
-        });
-      },
-      onHover: (value) {
-        print(value.position);
-        setState(() {
-          // _transDuration = 400;
+          _transDuration = 0;
           _left = value.localPosition.dx+5;
           _top = value.localPosition.dy+5;
         });
       },
-      child: RaisedButton(
-        padding: EdgeInsets.zero,
-        onPressed: () {},
-        color: Colors.transparent,
-        disabledColor: Colors.grey.withOpacity(0.25),
-        hoverColor: Colors.grey[300].withOpacity(0.1),
-        highlightColor: Colors.blue[600].withOpacity(0.25),
-        splashColor: Colors.blue[300].withOpacity(0.1),
-        elevation: 1,
-        disabledElevation: 0,
-        hoverElevation: 16,
-        focusElevation: 8,
-        highlightElevation: 8,
-        child: Card(
-          clipBehavior: Clip.antiAlias,
-          margin: EdgeInsets.zero,
-          elevation: 0,
-          child: Stack(
-            alignment: AlignmentDirectional.bottomCenter,
-            children: [
-              Container(
-                color: Colors.blue,
-                width: double.infinity,
-                height: double.infinity,
-              ),
-              Container(
-                color: Colors.white,
-                width: double.infinity,
-                height: 120,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "An application for Taking Note. Cloud Save and Markdown Text Styling Support.",
-                    style: TextStyle(fontSize: 16),
-                    overflow: TextOverflow.fade,
-                    softWrap: true,
+      onExit: (value) {
+        setState(() {
+          _elevation = 1;
+          _opacity = 0;
+        });
+      },
+      onHover: (value) {
+        // print("${value.localPosition}, $_cardWidth, $_cardHeight");
+        setState(() {
+          _transDuration = 500;
+          if (value.localPosition.dx+160+16+4 < _cardWidth) {
+            _left = value.localPosition.dx+5;
+          } else {
+            _left = _cardWidth-160-16-4;
+          }
+          if (value.localPosition.dy+48+16 < _cardHeight) {
+            _top = value.localPosition.dy+5;
+          } else {
+            _top = _cardHeight-48-16;
+          }
+        });
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        fit: StackFit.expand,
+        alignment: AlignmentDirectional.center,
+        children: [
+          Card(
+            clipBehavior: Clip.hardEdge,
+            margin: EdgeInsets.zero,
+            elevation: _elevation,
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+                Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  height: 120,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "An application for Taking Note. Cloud Save and Markdown Text Styling Support.",
+                      style: TextStyle(fontSize: 16),
+                      overflow: TextOverflow.fade,
+                      softWrap: true,
+                    ),
                   ),
                 ),
-              ),
-              AnimatedPositioned(
-                duration: Duration(milliseconds: _transDuration),
-                curve: Curves.fastLinearToSlowEaseIn,
-                left: _left,
-                top: _top,
-                height: 48,
-                width: 160,
-                child: AnimatedOpacity(
-                  duration: Duration(milliseconds: 200),
-                  opacity: _opacity,
-                  child: Card(
-                    margin: EdgeInsets.all(8),
-                    elevation: 4,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("Click for more detail"),
-                    ),
-                  )
+              ],
+            ),
+          ),
+          AnimatedPositioned(
+            duration: Duration(milliseconds: _transDuration),
+            curve: Curves.fastLinearToSlowEaseIn,
+            left: _left,
+            top: _top,
+            height: 48,
+            width: 160,
+            child: AnimatedOpacity(
+              duration: Duration(milliseconds: 200),
+              opacity: _opacity,
+              child: Card(
+                margin: EdgeInsets.all(8),
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Click for more detail"),
                 ),
               )
-            ],
-          ),
-        ),
-      ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
